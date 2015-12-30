baa069ac8050eef99a4ee3e07b0b5adf187179bf
zeng <597116245@qq.com>
2015-12-30 16:35:19 +0800

    Initial Commit

---
+++ YLSliderVC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YLSliderVC
+//
+//  Created by NaiveVDisk on 15/12/30.
+//  Copyright © 2015年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
